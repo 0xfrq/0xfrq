ddc0cc56de047501fe9f11f207ad2bc4fafe1aef
fariq <149790664+0xfrq@users.noreply.github.com>
2026-05-27 08:27:24 +0700

    Update update-readme.js

--- .github/scripts/update-readme.js
+++ .github/scripts/update-readme.js
@@ -3,7 +3,12 @@ const { createClient } = require('@supabase/supabase-js')
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_ANON_KEY,
+  {
+    realtime: {
+      transport: ws
+    }
+  }
 )
 
 async function main() {
